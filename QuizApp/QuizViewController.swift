cb1a383f0a88e2c6dc5d846e9c72fda5529b9fdf
toshiki0124 <ts.hiki1212@gmail.com>
2021-06-11 00:53:38 +0900

    FirstCommit

---
+++ QuizApp/QuizViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QuizViewController.swift
+//  QuizApp
+//
+//  Created by 中谷年希 on 2021/06/11.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
